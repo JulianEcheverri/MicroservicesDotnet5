8072cb5a3a3eb16cc5cb7f671568768b7881b069
Julian Echeverri <julianecheverri@outlook.com>
2021-11-14 21:19:10 -0500

    Grpc Startup register

--- Project/PlatformService/Startup.cs
+++ Project/PlatformService/Startup.cs
@@ -1,6 +1,8 @@
 using System;
+using System.IO;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -8,6 +10,7 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
 using PlatformService.AsyncDataServices;
 using PlatformService.Data;
+using PlatformService.SynchDataServices.Grpc;
 using PlatformService.SynchDataServices.Http;
 
 namespace PlatformService
@@ -49,6 +52,7 @@ namespace PlatformService
             services.AddScoped<IPlatformRepository, PlatformRepository>();
             services.AddHttpClient<ICommandDataClient, HttpCommandDataClient>();
             services.AddSingleton<IMessageBusClient, MessageBusClient>();
+            services.AddGrpc();
 
             Console.WriteLine($"--> CommandService Endpoint: {_configuration["CommandService"]}");
         }
@@ -72,6 +76,11 @@ namespace PlatformService
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+                endpoints.MapGrpcService<GrpcPlatformService>();
+                endpoints.MapGet("/protos/platforms.proto", async context =>
+                {
+                    await context.Response.WriteAsync(File.ReadAllText("Protos/platforms.proto"));
+                });
             });
 
             DbSetup.DbPopulation(app, _webHostEnvironment.IsProduction());
